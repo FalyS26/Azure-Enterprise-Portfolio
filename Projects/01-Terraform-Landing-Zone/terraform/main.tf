e109ec1bfbca239d4382041f62dacbb46420e145
falys26 <psylence212@gmail.com>
2026-07-09 09:35:43 -0400

    Add Azure Bastion subnet and host configuration

--- Projects/01-Terraform-Landing-Zone/terraform/main.tf
+++ Projects/01-Terraform-Landing-Zone/terraform/main.tf
@@ -117,4 +117,41 @@ resource "azurerm_subnet_network_security_group_association" "web" {
 resource "azurerm_subnet_network_security_group_association" "application" {
   subnet_id                 = azurerm_subnet.application.id
   network_security_group_id = azurerm_network_security_group.application.id
-}  
+}
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.4.0/26"]
+}
+resource "azurerm_public_ip" "bastion" {
+  name                = "pip-bastion-azure-portfolio-dev-eastus-001"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Azure Enterprise Portfolio"
+    Owner       = "Faly Sanchez"
+  }
+}
+
+resource "azurerm_bastion_host" "main" {
+  name                = "bas-azure-portfolio-dev-eastus-001"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Azure Enterprise Portfolio"
+    Owner       = "Faly Sanchez"
+  }
+}
